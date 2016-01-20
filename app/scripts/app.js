8a6aab5e7c108e877002a79857f7826c3f069888
Teri Pastorino <tpasto@comcast.net>
2016-01-19 17:06:45 -0800

    create album, collection, landing page templates and set up routig & state

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,30 @@
-angular.module('blocJams', []);
-/*blocJams argument is the module name, 
-second argument is an empty array used to inject dependencies */
+(function () {
+  function config($stateProvider, $locationProvider) {
+    $locationProvider
+      .html5Mode( {
+      enabled: true,
+      requireBase: false
+    });
+    
+    $stateProvider
+      .state('landing', {
+        url: '/',
+      templateUrl: '/templates/landing.html'
+    })
+    
+    .state('album', {
+      url: '/album',
+      templateUrl:'/templates/album.html'
+    });
+  }
 
+
+  angular
+    .module('blocJams', [ui.router])
+    .config(config);
+})();
+    /*
+    1. blocJams argument is the module name, 
+    2. second argument is an empty  array used to inject dependencies
+    3. set up state configuration using provider (=service)
+    */
